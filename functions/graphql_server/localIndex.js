61618452f205e8b38b35bea980185c2b95e5792b
Dennis Chung <stvreumi@livemail.tw>
2020-02-04 14:59:18 +0800

    localIndex.js: run graphql server locally to test

--- functions/graphql_server/localIndex.js
+++ functions/graphql_server/localIndex.js
@@ -1,14 +1,17 @@
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
 
 const apolloServer = require('./src');
 
 // Start our server if we're not in a test env.
 // if we're in a test env, we'll manually start it in a test
 if (process.env.NODE_ENV !== 'test') {
-  apolloServer
-    .listen({ port: 4001 })
-    .then(({ url }) => console.log(`app running at ${url}`));
+  apolloServer({ admin })
+    .listen({ port: 4001 }, () =>
+      console.log('app running at port 4001')
+    );
 }
