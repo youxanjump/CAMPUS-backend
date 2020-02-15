9d9861ce7c6e4c2c93f68018d6c180622837d5ef
Dennis Chung <stvreumi@livemail.tw>
2020-02-15 15:46:44 +0800

    Write detailed comment in ahthorization.js

--- functions/graphql_server/src/authorization.js
+++ functions/graphql_server/src/authorization.js
@@ -1,14 +1,27 @@
+/** @module authorization */
 const { ForbiddenError } = require('apollo-server');
 const { skip } = require('graphql-resolvers');
 
 /**
- * Check if user is authorized.
+ * A resover. Check if user is authorized.
+ * Use `combineResolvers` in `graphql-resolvers` to combine
+ * with other resolvers.
+ * @todo Understand why returning error instance will work.
+ * reference: 
+ * https://www.apollographql.com/docs/graphql-tools/resolvers/#graphql-resolvers
+ * https://www.robinwieruch.de/graphql-apollo-server-tutorial#authorization-with-graphql-and-apollo-server
+ * @returns {undefined} skip, go to executute next resolvers.
+ * If not login, return ForbiddenError forbid not login users
  */
 module.exports.isAuthenticated = (_, __, { me }) =>
   (me ? skip : new ForbiddenError('User is not login'));
 
 /**
- * if user want to modify the tag, check if user is the tag Owner.
+ * A resover. If user want to modify the tag, check if user is the tag Owner.
+ * Use `combineResolvers` in `graphql-resolvers` to combine
+ * with other resolvers.
+ * @returns {undefined} skip, go to executute next resolvers
+ * @throws {ForbiddenError} forbid not authorize users
  */
 module.exports.isTagOwner = (_, { data }, { me, dataSources }) => {
   if (data.modify) {
