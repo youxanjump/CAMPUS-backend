a9645e8970b0a5bf8efa9a35e385dbd9da98afa5
Dennis Chung <stvreumi@livemail.tw>
2020-08-28 16:58:07 +0800

    Add graphql test server for auth testing

---
+++ functions/src/datasources/__test__/apolloTestServer.js
@@ -0,0 +1,43 @@
+const { ApolloServer } = require('apollo-server-express');
+
+const typeDefs = require('../../schema');
+const resolvers = require('../../resolvers');
+const FirebaseAPI = require('../firebase');
+const { fakeUserInfo } = require('./testUtils');
+
+/**
+ * Apollo server instance
+ * @param {object} {admin} firebase admin SDK
+ * @returns {ApolloServer} ApolloServer with config
+ */
+function apolloTestServer({ admin, logIn }) {
+  const dataSources = () => ({
+    firebaseAPI: new FirebaseAPI({ admin }),
+  });
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources,
+    formatError: error => {
+      console.log('error');
+      console.log(error);
+      return error;
+    },
+    context: async () => {
+      if (logIn) {
+        return fakeUserInfo;
+      }
+      return {
+        logIn: false,
+        uid: 'anonymous',
+        displayName: 'anonymous',
+      };
+    },
+  });
+
+  // server.applyMiddleware({ app, path: '/' });
+  return server;
+}
+
+module.exports = apolloTestServer;
